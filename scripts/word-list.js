c4b60ac72e211375176aa99e1de0770bd1d16009
BLISMING <blisming@gmail.com>
2023-09-10 17:54:35 +0700

    dasaekka

---
+++ scripts/word-list.js
@@ -0,0 +1,148 @@
+const wordList = [
+    {
+        word: "wizra",
+        hint: "teriakan, garang"
+    },
+       {
+
+        word: "qarin",
+        hint: "kalem, mungil, imut"
+    },
+    {
+        word: "susi",
+        hint: "mungil, gak iso meneng"
+    },
+    {
+        word: "mamat",
+        hint: "India, Belanda"
+    },
+    {
+        word: "aat",
+        hint: "Sahabatnya Mamat"
+    },
+    {
+        word: "akmal",
+        hint: "Oyek kecil"
+    },
+    {
+        word: "mifta",
+        hint: "Wibu, cewek, duduk bareng si cewek karate"
+    },
+    {
+       word: "nayla",
+        hint: "Si paling duta, gandengannya Nazhril"
+    },
+    {
+        word: "adit",
+        hint: "Isine fyp tiktoke Motor tok"
+    },
+    {
+        word: "gerald",
+        hint: "Kamera ne apik masio android"
+    },
+    {
+        word: "dhenil",
+        hint: "Kuda Nil"
+    },
+    {
+        word: "faris",
+        hint: "duduk bareng Kuda Nil"
+    },
+    {
+        word: "arief",
+        hint: "Si Paling Seniman"
+    },
+    {
+        word: "ilyas",
+        hint: "Wes seng gawe website iki"
+    },
+    {
+        word: "ryan",
+        hint: "Pengen dicelok Rayen"
+    },
+    {
+        word: "mala",
+        hint: "Kelas e sering digawe tempat mesra"
+    },
+    {
+        word: "made",
+        hint: "Basketan ambek pacaran tok "
+    },
+    {
+        word: "nadinda",
+        hint: "Tulisan paling apik neng kelas"
+    },
+    {
+        word: "reza s",
+        hint: "Jipook"
+    },
+    {
+        word: "dhasa",
+        hint: "Wong Pusat"
+    },
+    {
+        word: "nuril",
+        hint: "Temanya si kecil aktif tuh"
+    },
+    {
+        word: "dewa",
+        hint: "wes anak rohis lanangan"
+    },
+    {
+        word: "rifqi",
+        hint: "Kidal"
+    },
+    {
+        word: "levina",
+        hint: "suka dylan nih"
+    },
+    {
+        word: "alisya",
+        hint: "Sepuh Pramuka"
+    },
+    {
+        word: "aulia",
+        hint: "Duduknya barengan sama mbak levina"
+    },
+    {
+        word: "rangga",
+        hint: "Bismillah Casis"
+    },
+    {
+        word: "choiril",
+        hint: "Guyune koyok kademen"
+    },
+    {
+        word: "reza r",
+        hint: "Duduk bareng aat ambek duwe kembaran Nama, jenenge Jipook"
+    },
+    {
+        word: "rafa",
+        hint: "Pedagang Dasaekka"
+    },
+    {
+        word: "jihan",
+        hint: "Hpan+turu lek jamkos"
+    },
+    {
+        word: "ima",
+        hint: "duduknya barengan sama zarah"
+    },
+    {
+        word: "azzahra",
+        hint: "Duduknya barengan sama Ima"
+    },
+    {
+        word: "elsa",
+        hint: "Satu sirkel sama Nayla "
+    },
+    {
+        word: "andin",
+        hint: "duduknya sama sepuh pramuka"
+    },
+    {
+        word: "safina",
+        hint: "Duduknya barengan sama sekertaris cewek "
+    },
+   
+];
